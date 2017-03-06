cf92d68976aadea20d29931a1b22b17a49d1910b
Troy Pracy <troyp7@gmail.com>
2017-03-06 15:09:57 +1100

    change should-equal to macro for better readability

--- ls-test.el
+++ ls-test.el
@@ -3,49 +3,57 @@
 (require 'ert)
 (require 'fn)
 
-(cl-flet
-    ((should-equal (a b)
-                   (should (equal a b)))
-     (should-eql (a b)
-                 (should (eql a b)))
-     (should= (a b)
-              (should (= a b))))
-
+(cl-macrolet ((should-equal
+               (expr keyword result)
+               (if (eq keyword :result)
+                   `(should (equal ,expr ,result))
+                 (error "expected :result"))))
 
   (ert-deftest test-ls-range ()
     "Test `ls-range'."
 
     (should-equal (ls-range to 5)
+                  :result
                   '(0 1 2 3 4 5))
 
     (should-equal (ls-range below 5)
+                  :result
                   '(0 1 2 3 4))
 
     (should-equal (ls-range from 1 to 10 by 2)
+                  :result
                   '(1 3 5 7 9))
 
     (should-equal (ls-range from 1 to 8 by 1.5)
+                  :result
                   '(1 2.5 4.0 5.5 7.0))
 
     (should-equal (ls-range from 3 downto -3)
+                  :result
                   '(3 2 1 0 -1 -2 -3))
 
     (should-equal (ls-range from 1 to 10 when (oddp i))
+                  :result
                   '(1 3 5 7 9))
 
     (should-equal (ls-range from 1 until (> (* i i) 50))
+                  :result
                   '(1 2 3 4 5 6 7))
 
     (should-equal (ls-range in '(a b c d e) by 'cddr)
+                  :result
                   '(a c e))
 
     (should-equal (ls-range on '(a b c d))
+                  :result
                   '((a b c d) (b c d) (c d) (d)))
 
     (should-equal (ls-range on (ls-range to 3))
+                  :result
                   '((0 1 2 3) (1 2 3) (2 3) (3)))
 
     (should-equal (-map 'string (ls-range from ?A to ?K))
+                  :result
                   '("A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K"))
     )
 
@@ -53,15 +61,19 @@
     "Test `ls-cons-if-t'."
 
     (should-equal (ls-cons-if-t 'a '(b c))
+                  :result
                   '(a b c))
 
     (should-equal (ls-cons-if-t nil '(b c))
+                  :result
                   '(b c))
 
     (should-equal (ls-cons-if-t 2 3)
+                  :result
                   '(2 . 3))
 
     (should-equal (ls-cons-if-t nil 3)
+                  :result
                   3)
     )
 
@@ -69,6 +81,7 @@
     "Test `ls-zero-when'."
     (should-equal (ls-zero-when (fn (zerop (mod <> 3)))
                                 (ls-range from 1 to 10))
+                  :result
                   '(nil nil 3 nil nil 6 nil nil 9 nil))
     )
 
@@ -76,6 +89,7 @@
     "Test `ls-zero-unless'."
     (should-equal (ls-zero-unless (fn (zerop (mod <> 3)))
                                   (ls-range from 1 to 10))
+                  :result
                   '(1 2 nil 4 5 nil 7 8 nil 10))
     )
 
@@ -83,14 +97,16 @@
     "Test `ls-*-before'."
 
     (let ((list '(1 2 3 4 5 6 7 8 9 10)))
-      (should-equal list
-                    (-concat (ls-take-before '7 list)
-                             (ls-drop-before '7 list))))
+      (should-equal (-concat (ls-take-before '7 list)
+                             (ls-drop-before '7 list))
+                    :result
+                    list))
 
     (let ((list '((1 2) "foo" ?a (3 . 4))))
-      (should-equal list
-                    (-concat (ls-take-before ?a list)
-                             (ls-drop-before ?a list))))
+      (should-equal (-concat (ls-take-before ?a list)
+                             (ls-drop-before ?a list))
+                    :result
+                    list))
     )
 
   )
